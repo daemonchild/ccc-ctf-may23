ff0eececd567a9ac896f59bf0b950ab3591d6e4a
Daemonchild (MW0KKR) <tom@daemonchild.com>
2023-05-17 14:03:07 +0100

    ..

--- setup-scripts/guacamole/setup.sh
+++ setup-scripts/guacamole/setup.sh
@@ -11,11 +11,11 @@ docker pull nginx:latest
 docker run --name guacd-151 -d guacamole/guacd
 
 docker run -d --name guacamole-151 --link guacd-151:guacd \
-	-e MYSQL_HOSTNAME=mysql-ccc-ctf-may23.mysql.database.azure.com \
+	-e MYSQL_HOSTNAME=mysqlsvr.mysql.database.azure.com \
 	-e MYSQL_PORT=3306 \
 	-e MYSQL_DATABASE=guacamoledb \
-	-e MYSQL_USER=guacdbuser@mysql-ccc-ctf-may23 \
-	-e MYSQL_PASSWORD=tNTUwfshM93P1w66_1jalkDDZSgYe5S9ySVT6D_mu \
+	-e MYSQL_USER=mysqluser@ysqlsvr \
+	-e MYSQL_PASSWORD=mysqlpassword \
 	-e MYSQL_SSL_MODE=disabled \
 	guacamole/guacamole
 
