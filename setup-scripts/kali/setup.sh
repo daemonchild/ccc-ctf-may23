c735b945562cf695ad7bd58dfd20b8766794f2ce
Daemonchild (MW0KKR) <tom@daemonchild.com>
2023-05-17 08:50:38 +0100

    .

--- setup-scripts/kali/setup.sh
+++ setup-scripts/kali/setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+export LANG=en_GB.utf8
+export LANGUAGE=
+export LC_CTYPE="en_GB.utf8"
+export LC_NUMERIC="en_GB.utf8"
+export LC_TIME="en_GB.utf8"
+export LC_COLLATE="en_GB.utf8"
+export LC_MONETARY="en_GB.utf8"
+export LC_MESSAGES="en_GB.utf8"
+export LC_PAPER="en_GB.utf8"
+export LC_NAME="en_GB.utf8"
+export LC_ADDRESS="en_GB.utf8"
+export LC_TELEPHONE="en_GB.utf8"
+export LC_MEASUREMENT="en_GB.utf8"
+export LC_IDENTIFICATION="en_GB.utf8"
+export LC_ALL=
+
 apt update
 apt full-upgrade -y
 apt install -y kali-linux-default
