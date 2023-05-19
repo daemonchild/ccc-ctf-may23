d1c89e4f3f822637da3829596388c1f2001c628e
Daemonchild (MW0KKR) <tom@daemonchild.com>
2023-05-19 12:35:19 +0100

    Firewalls

--- azure-scripts/ctfbuild.sh
+++ azure-scripts/ctfbuild.sh
@@ -563,3 +563,38 @@ buildkali () {
 }
 
 
+kalifirewall () {
+
+    local team=$1
+
+    local vmname="${vmprefix}-Kali-${team}"
+
+    local subnetname="${challengenetprefix}-${team}"
+    local subnet="${twooctets}.${team}.0/24"
+
+    while read LINE ; do 
+    
+        priority=$(echo LINE | cut -d "," -f 1)
+        source=$(echo LINE | cut -d "," -f 2 | sed "s/x/$team/g")
+        dest=$(echo LINE | cut -d "," -f 3)
+        destport=$(echo LINE | cut -d "," -f 4)
+        action=$(echo LINE | cut -d "," -f 6)
+        name=$(echo LINE | cut -d "," -f 7)
+        protocol="Tcp"
+
+        echo $source
+
+        #az network nsg rule create \
+        #--resource-group $resgrp \
+        #--nsg-name "nsg-${vmname}" \
+        #--name $name \
+        #--access $action \
+        #--protocol $protocol \
+        #--direction Inbound \
+        #--priority $priority \
+        #--source-address-prefix $source \
+        #--source-port-range "*" \
+        #--destination-address-prefix $dest \
+        #--destination-port-range $destport
+
+}
