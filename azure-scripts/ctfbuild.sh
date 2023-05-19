9b5b4d5c4ee5f9d1df8980de1103c825755f500b
Daemonchild (MW0KKR) <tom@daemonchild.com>
2023-05-19 12:50:15 +0100

    sss

--- azure-scripts/ctfbuild.sh
+++ azure-scripts/ctfbuild.sh
@@ -596,6 +596,6 @@ kalifirewall () {
         --source-port-range "*" \
         --destination-address-prefix $dest \
         --destination-port-range $destport
-    done < "kali-firewall-rules.csv"
+    done < "./azure-scripts/kali-firewall-rules.csv"
 
 }
